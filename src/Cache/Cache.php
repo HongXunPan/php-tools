e335b16ae086b6d6dbda7b3c96bdc386f43f6aaf
HongXunPan <me@kangxuanpeng.com>
2024-02-06 16:52:18 +0800

    缓存

---
+++ src/Cache/Cache.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace HongXunPan\Tools\Cache;
+
+class Cache
+{
+    public function remember()
+    {
+
+    }
+
+    public function get()
+    {
+
+    }
+
+    public function set()
+    {
+
+    }
+}
