8e88d7dcd1220b6ec9968236c699ba982320084d
HongXunPan <me@kangxuanpeng.com>
2024-02-07 14:03:47 +0800

    缓存 remember

--- src/Cache/Cache.php
+++ src/Cache/Cache.php
@@ -2,20 +2,81 @@
 
 namespace HongXunPan\Tools\Cache;
 
+use HongXunPan\DB\Redis\Redis;
+
 class Cache
 {
-    public function remember()
-    {
+    /** @var null|static $instance */
+    private static $instance = null;
 
+    private function __construct()
+    {
+        //
     }
 
-    public function get()
+    private function __clone()
     {
-
+        //
     }
 
-    public function set()
+    /**
+     * @return static|null
+     */
+    public static function getInstance()
     {
+        //判断$instance
+        if (self::$instance === null) {
+            self::$instance = new static();
+        }
+        return self::$instance;
+    }
 
+    /** @var \Redis $redis */
+    private $redis;
+    private $prefix = 'cache:';
+
+    public function setConfig($prefix = 'cache:', $redis = null)
+    {
+        $this->prefix = $prefix;
+        if ($redis instanceof \Redis) {
+            $this->redis = $redis;
+        } else {
+            $this->redis = Redis::connection();
+        }
+    }
+
+    private $getCache = true;
+    public function setCacheMode($getCache = true)
+    {
+        $this->getCache = $getCache;
+        return $this;
+    }
+
+    public static function remember($redisKey, $ttl, callable $function)
+    {
+        $cache = static::get($redisKey);
+        if ($cache !== false) {
+            return $cache;
+        }
+        $res = $function();
+        static::set($redisKey, $res, $ttl);
+        return $cache;
+    }
+
+    public static function get($redisKey)
+    {
+        $instance = static::getInstance();
+        if (!$instance->getCache) {
+            return false;
+        }
+        $redisKey = $instance->prefix . $redisKey;
+        return $instance->redis->get($redisKey);
+    }
+
+    public static function set($redisKey, $data, $ttl)
+    {
+        $instance = static::getInstance();
+        $redisKey = $instance->prefix . $redisKey;
+        $instance->redis->set($redisKey, $data);
     }
 }
