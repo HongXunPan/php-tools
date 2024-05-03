f39d9be00cd36d68968505aa68cef50da42b4391
HongXunPan <me@kangxuanpeng.com>
2024-05-03 12:05:35 +0800

    es connection

---
+++ src/ElasticSearch/ElasticSearch.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace HongXunPan\Tools\ElasticSearch;
+
+use Elastic\Elasticsearch\ClientBuilder;
+use HongXunPan\DB\DBContract;
+
+class ElasticSearch extends DBContract
+{
+    protected function ping($connection)
+    {
+        return $connection;
+    }
+
+    protected function connect(array $config)
+    {
+        return ClientBuilder::create()           // 实例化 ClientBuilder
+        ->setHosts($config)      // 设置主机信息
+        ->build();
+    }
+
+    public static function setConfig(array $config = [], $connectName = 'default', array $options = [])
+    {
+        parent::saveConfig($config, $connectName);
+    }
+}
---
+++ src/ElasticSearch/ElasticSearchConnection.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace HongXunPan\Tools\ElasticSearch;
+
+use HongXunPan\DB\DBConnectionContract;
+
+class ElasticSearchConnection extends DBConnectionContract
+{
+
+    protected function recordLog($name, $arguments)
+    {
+        // TODO: Implement recordLog() method.
+    }
+}
