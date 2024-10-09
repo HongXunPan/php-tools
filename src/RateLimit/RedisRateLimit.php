b14d0dc49db47d65f00127e7b92a743eebc58a67
HongXunPan <me@kangxuanpeng.com>
2024-10-09 11:23:45 +0800

    rate limit

---
+++ src/RateLimit/RedisRateLimit.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace HongXunPan\Tools\RateLimit;
+
+/**
+ * redis 限流器
+ */
+class RedisRateLimit
+{
+
+}
---
+++ src/ValueShare/FileToKey.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace HongXunPan\Tools\ValueShare;
+
+class Ftok
+{
+
+}
