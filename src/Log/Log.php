f3bac084e4f7c4880fcb2ee369bb9ccee31804b8
HongXunPan <me@kangxuanpeng.com>
2025-02-27 16:08:24 +0800

    log tools use trait

--- src/Log/Log.php
+++ src/Log/Log.php
@@ -3,14 +3,23 @@
 namespace HongXunPan\Tools\Log;
 
 use HongXunPan\Tools\Abstracts\SingletonAbstract;
+use Psr\Log\LoggerInterface;
+use Psr\Log\LoggerTrait;
 
-class Log extends SingletonAbstract
+class Log extends SingletonAbstract implements LoggerInterface
 {
+    use LoggerTrait;
     private $logPath = __DIR__ . '/../../../logs';
 
-    public static function info($msg, $data = [])
+    public function setLogPath($logPath)
     {
-        self::getInstance()->write('info', $msg, $data);
+        if (!is_dir($logPath)) {
+            mkdir($logPath, 0777, true);
+        }
+        if (!is_writable($logPath)) {
+            throw new \Exception("log path is not writable");
+        }
+        $this->logPath = $logPath;
     }
 
     protected function write($level, $msg, $data = [])
@@ -22,6 +31,11 @@ class Log extends SingletonAbstract
             'time' => date('Y-m-d H:i:s'),
         ];
         $log = json_encode($log, JSON_UNESCAPED_UNICODE);
-        @file_put_contents($this->logPath . '/' . date('Y-m-d') . '.log', $log . PHP_EOL, FILE_APPEND);
+        @file_put_contents($this->logPath . '/' . $level . date('Y-m-d') . '.log', $log . PHP_EOL, FILE_APPEND);
+    }
+
+    public function log($level, $message, array $context = [])
+    {
+        // TODO: Implement log() method.
     }
 }
