79452ab00dea0e563b6bed566066a5434dfda753
HongXunPan <me@kangxuanpeng.com>
2025-03-08 14:12:07 +0800

    log tools channel

--- src/Log/Log.php
+++ src/Log/Log.php
@@ -6,10 +6,15 @@ use HongXunPan\Tools\Abstracts\SingletonAbstract;
 use Psr\Log\LoggerInterface;
 use Psr\Log\LoggerTrait;
 
+/**
+ * @method static void log($level, $message, array $context = [])
+ */
 class Log extends SingletonAbstract implements LoggerInterface
 {
     use LoggerTrait;
 
+    protected $channel = [];
+    private $fileName = '';
     private $logPath = __DIR__ . '/../../../logs';
 
     public function setLogPath($logPath)
@@ -21,6 +26,31 @@ class Log extends SingletonAbstract implements LoggerInterface
             throw new \Exception("log path is not writable");
         }
         $this->logPath = $logPath;
+    }
+
+    public function getLogPath()
+    {
+        return $this->logPath;
+    }
+
+    public static function channel($channel = '')
+    {
+        if (!$channel) {
+            return self::getInstance();
+        }
+        if (isset(self::$instance[$channel])) {
+            return self::$instance[$channel];
+        }
+        $log = new static();
+        self::$instance[$channel] = $log;
+        $log->setLogPath(self::getInstance()->getLogPath());
+        $log->channel = $channel;
+        return $log;
+    }
+
+    public static function __callStatic($name, $arguments)
+    {
+        return self::getInstance()->$name(...$arguments);
     }
 
     protected function write($level, $msg, $data = [])
@@ -35,7 +65,8 @@ class Log extends SingletonAbstract implements LoggerInterface
 //        @file_put_contents($this->logPath . '/' . $level . date('Y-m-d') . '.log', $log . PHP_EOL, FILE_APPEND);
 
         $time = date('Y-m-d H:i:s');
-        $log = $time . ' - ' . posix_getpid();
+        $day = date('Ymd');
+        $log = "[" . strtoupper($level) . "] " . $time . ' - ' . posix_getpid();
         if (php_sapi_name() != 'cli') {
             $uri = $_SERVER['REQUEST_URI'];
             $host = $_SERVER['HTTP_HOST'];
@@ -50,7 +81,8 @@ class Log extends SingletonAbstract implements LoggerInterface
             $log .= PHP_EOL . json_encode($data, JSON_PRETTY_PRINT);
         }
         $log .= PHP_EOL;
-        @file_put_contents($this->logPath . '/' . $level . substr($time, 0, 10) . '.log', $log . PHP_EOL, FILE_APPEND);
+        $fileName = $this->channel ? $this->channel . '-' . $day . "log" : $level . "-" . $day . "log";
+        @file_put_contents($this->logPath . '/' . $fileName, $log . PHP_EOL, FILE_APPEND);
     }
 
     /** @noinspection PhpElementIsNotAvailableInCurrentPhpVersionInspection
