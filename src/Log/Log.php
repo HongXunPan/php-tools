24129922facc36c81ea2b5a5f5ba9f2c9112ea70
HongXunPan <me@kangxuanpeng.com>
2025-03-08 12:02:31 +0800

    log tools

--- src/Log/Log.php
+++ src/Log/Log.php
@@ -9,6 +9,7 @@ use Psr\Log\LoggerTrait;
 class Log extends SingletonAbstract implements LoggerInterface
 {
     use LoggerTrait;
+
     private $logPath = __DIR__ . '/../../../logs';
 
     public function setLogPath($logPath)
@@ -24,18 +25,36 @@ class Log extends SingletonAbstract implements LoggerInterface
 
     protected function write($level, $msg, $data = [])
     {
-        $log = [
-            'level' => $level,
-            'msg' => $msg,
-            'data' => $data,
-            'time' => date('Y-m-d H:i:s'),
-        ];
-        $log = json_encode($log, JSON_UNESCAPED_UNICODE);
-        @file_put_contents($this->logPath . '/' . $level . date('Y-m-d') . '.log', $log . PHP_EOL, FILE_APPEND);
+//        $log = [
+//            'level' => $level,
+//            'msg' => $msg,
+//            'data' => $data,
+//            'time' => date('Y-m-d H:i:s'),
+//        ];
+//        $log = json_encode($log, JSON_UNESCAPED_UNICODE);
+//        @file_put_contents($this->logPath . '/' . $level . date('Y-m-d') . '.log', $log . PHP_EOL, FILE_APPEND);
+
+        $time = date('Y-m-d H:i:s');
+        $log = $time . ' - ' . posix_getpid();
+        if (php_sapi_name() != 'cli') {
+            $uri = $_SERVER['REQUEST_URI'];
+            $host = $_SERVER['HTTP_HOST'];
+            $http = $_SERVER['REQUEST_SCHEME'];
+            $url = $http . '://' . $host . $uri;
+            $log .= ' - ' . $url;
+        }
+        if ($msg) {
+            $log .= PHP_EOL . $msg;
+        }
+        if ($data) {
+            $log .= PHP_EOL . json_encode($data, JSON_PRETTY_PRINT);
+        }
+        $log .= PHP_EOL;
+        @file_put_contents($this->logPath . '/' . $level . substr($time, 0, 10) . '.log', $log . PHP_EOL, FILE_APPEND);
     }
 
     public function log($level, $message, array $context = [])
     {
-        // TODO: Implement log() method.
+        $this->write($level, $message, $context);
     }
 }
